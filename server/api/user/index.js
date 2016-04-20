93cbf590ed7402dbcfbc5df52941cf8858c3a459
Sandip <sandip.lakum5@gmail.com>
2016-04-20 16:33:27 +0530

    define new route forgot,ragister,reset,forgot,GetProduct

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -1,11 +1,18 @@
 'use strict';
 var express=require("express");
 var routes=express.Router();
+var session = require('express-session');
+
 
 
 var controllr = require("./controller/user.controler.js");
 
 routes.post('/register',controllr.insert);
 routes.post('/login',controllr.login);
+routes.post('/forgot_pass', controllr.forgot);
+routes.post('/reset',controllr.resetpass);
+routes.get('/logout',controllr.logout);
+//routes.get('/home',controllr.homepage);
+routes.post('/getProduct',controllr.getProduct);
 	
 module.exports=routes;
